778abca41a7aad234777286a5b9476f9ade64c52
Louise Reid <l_reid@hotmail.co.uk>
2018-04-15 17:17:12 +0100

    boilerplate setup

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+
+class App extends React.Component {
+  
+  componentWillMount(){
+    const config = {
+      apiKey: 'AIzaSyA-5vhfqALyzPngLCNyyR-lX1uywt5YhCk',
+      authDomain: 'employee-management-aec6f.firebaseapp.com',
+      databaseURL: 'https://employee-management-aec6f.firebaseio.com',
+      projectId: 'employee-management-aec6f',
+      storageBucket: '',
+      messagingSenderId: '1014795133455'
+    };
+    firebase.initializeApp(config);
+  }
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    )
+  }
+}
+
+export default App;
